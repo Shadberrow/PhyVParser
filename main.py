e9bbda6dd4da78017382aade375dc0d1378eed2e
pc01 <pc01@pc01ui-iMac-Pro.local>
2019-10-17 17:56:06 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,30 @@
+import re
+
+def _parse_line(line):
+    for key, rx in rx_dict.items():
+        match = rx.search(line)
+        if match:
+            return key, match
+    return None, None
+
+
+def _parse_file(filepath):
+    data = []
+    with open(filepath, 'r') as file:
+        line = file.readline()
+        while line:
+            key, match = _parse_line(line)
+            print(key, match)
+            line = file.readline()
+    return data
+
+rx_dict = {
+    'input': re.compile(r'input(?P<input>.*);'),
+    'output': re.compile(r'output(?P<output>.*);'),
+    'inout': re.compile(r'inout(?P<inout>.*);'),
+    'wire': re.compile(r'wire(?P<wire>.*);')
+}
+
+if __name__ == '__main__':
+    filepath = 'sample01.v'
+    data = _parse_file(filepath)
