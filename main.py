c37c31e82e2ea8fa3a4a76b758e8238a7bc9d7a8
pc01 <pc01@pc01ui-iMac-Pro.local>
2019-10-21 05:05:28 +0900

    managed to get repeated values

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import re
+import collections
+import fileinput
+import random
+import string
+import os, sys
 
 def _parse_line(line):
     for key, rx in rx_dict.items():
@@ -17,16 +22,16 @@ def _parse_file(filepath):
 
             if key == 'input':
                 input = match.group('input')
-                print(input)
+                data.append(input)
             if key == 'inout':
                 input = match.group('inout')
-                print(input)
+                data.append(input)
             if key == 'output':
                 input = match.group('output')
-                print(input)
+                data.append(input)
             if key == 'wire':
                 input = match.group('wire')
-                print(input)
+                data.append(input)
 
             line = file.readline()
 
@@ -42,3 +47,10 @@ rx_dict = {
 if __name__ == '__main__':
     filepath = 'sample01.v'
     data = _parse_file(filepath)
+    # print(data)
+
+    data = map(lambda x: x.lower(), data)
+    data = map(lambda x: x.strip(), data)
+
+    repeated = [item for item, count in collections.Counter(data).items() if count > 1]
+    print(repeated)
