5344eb33dc9785d49bc7e74221310ada3e9475e4
pc01 <pc01@pc01ui-iMac-Pro.local>
2019-10-23 18:33:36 +0900

    added VerilogModule to store each module inputs outputs etc. Parse a verilog file down to separate VerilogModules

--- main.py
+++ main.py
@@ -1,9 +1,50 @@
 import re
 import collections
-import fileinput
-import random
-import string
 import os, sys
+import json
+
+class VerilogModule:
+    def __init__(self):
+        self.name = 'default'
+        self.inputs = [];
+        self.outputs = [];
+        self.wires = [];
+        self.inouts = [];
+
+    def __init__(self, name):
+        self.name = name
+        self.inputs = [];
+        self.outputs = [];
+        self.wires = [];
+        self.inouts = [];
+
+    def addInput(self, input):
+        self.inputs.append(input)
+
+    def addInout(self, name):
+        self.inouts.append(name)
+
+    def addOutput(self, name):
+        self.outputs.append(name)
+
+    def addWire(self, name):
+        self.wires.append(name)
+
+    def __str__(self):
+        info = {
+            'name': self.name,
+            'inputs': self.inputs,
+            'inouts': self.inouts,
+            'outputs': self.outputs,
+            'wires': self.wires
+        }
+        return json.dumps(info)
+
+def _get_only_name(text):
+    x = re.sub('[\(\[].*?[\)\]]', '', text)
+    x = re.sub(' +', ' ', x)
+    x = re.sub(';', '', x)
+    return x.strip()
 
 def _parse_line(line):
     for key, rx in rx_dict.items():
@@ -12,60 +53,101 @@ def _parse_line(line):
             return key, match
     return None, None
 
+def _find_modules_in(filepath):
+    modules = []; module = '';
+    do_read_module = False;
+    with open(filepath, 'r') as file:
+        line = file.readline()
+        while line:
+            if do_read_module:
+                module += line
+
+            match = re.compile(r'\bmodule\b | \bendmodule\b', flags=re.I | re.X)
+            res = match.search(line)
+            if res:
+                if res.group() == 'module':
+                    do_read_module = True
+                    module += line
+                elif res.group() == 'endmodule':
+                    do_read_module = False
+                    modules.append(module)
+                    module = ''
+            line = file.readline()
+    return modules
 
 def _parse_file(filepath):
-    data = []
+    name = ''; newMod = VerilogModule(''); modules = [];
+
     with open(filepath, 'r') as file:
         line = file.readline()
         while line:
             key, match = _parse_line(line)
 
+            if key == 'module':
+                name = _get_only_name(match.group(key))
+                newMod = VerilogModule(name)
+                modules.append(newMod)
             if key == 'input':
-                input = match.group('input')
-                data.append(input)
+                name = _get_only_name(match.group(key))
+                newMod.addInput(name);
             if key == 'inout':
-                input = match.group('inout')
-                data.append(input)
+                name = _get_only_name(match.group(key))
+                newMod.addInout(name);
             if key == 'output':
-                input = match.group('output')
-                data.append(input)
+                name = _get_only_name(match.group(key))
+                newMod.addOutput(name);
             if key == 'wire':
-                input = match.group('wire')
-                data.append(input)
+                name = _get_only_name(match.group(key))
+                newMod.addWire(name);
 
             line = file.readline()
-
-    return data
+    return modules
 
 rx_dict = {
-    'input': re.compile(r'input(?P<input>.*);'),
-    'output': re.compile(r'output(?P<output>.*);'),
-    'inout': re.compile(r'inout(?P<inout>.*);'),
-    'wire': re.compile(r'wire(?P<wire>.*);')
+    'input'  : re.compile(r'input(?P<input>.*;)'),
+    'output' : re.compile(r'output(?P<output>.*;)'),
+    'inout'  : re.compile(r'inout(?P<inout>.*;)'),
+    'wire'   : re.compile(r'wire(?P<wire>.*;)'),
+    'module' : re.compile(r'\bmodule\b(?P<module>.*\w)')
 }
 
 if __name__ == '__main__':
-    filepath = 'sample01.v'
-    data = _parse_file(filepath)
-    # print(data)
+    modules = []
 
-    data = map(lambda x: x.lower(), data)
-    data = map(lambda x: x.strip(), data)
+    # filepath = 'tx_handler_bad.phy.v'
+    # filepath = 'sample01.v'
+    filepath = 'test_multiple_modules.phy.v'
+    modules = _parse_file(filepath);
 
-    repeated = [item for item, count in collections.Counter(data).items() if count > 1]
-    print(repeated)
+    for mod in modules:
+        print(mod)
 
-    # with fileinput.FileInput(filepath, inplace=True, backup='.bak') as file:
-    #     for line in file:
-    #         for x in repeated:
-    #             if x in line:
-    #                 line.replace(x, '**Q')
+
+
+    # data = map(lambda x: x.lower(), data)
+    # repeated = [item for item, count in collections.Counter(data).items() if count > 1]
+    # print('List of repeated names:')
+    # print(repeated)
+
+    # found_modules = _find_modules_in(filepath)
+    #
+    # for module in found_modules:
+    #     print(module)
+    #     # for line in module:
+    #         # print(_find_key_names(line))
+
+    # res = re.compile(r'\bmodule\b | \bendmodule\b', flags=re.I | re.X)
+    # print(res.search('asdfdsf A ('))
 
     # input = open(filepath)
-    # output = open('out', 'w')
-    # for x in repeated:
-    #     for s in input.xreadlines(  ):
-    #         if x in s:
-    #             output.write(s.replace(x, '**Q'))
-    #         else:
-    #             output.write(s)
+    # output = open(filepath+'_fixed', 'w')
+    #
+    # for s in input.xreadlines():
+    #     new_str = s;
+    #     for x in repeated:
+    #         if x in new_str:
+    #             new_str = re.sub(r'\b'+x+r'\b', '/______GENERATED_________'+x , new_str)
+    #     output.write(new_str)
+    #
+    # input.close()
+    # output.close()
---
+++ test_multiple_modules.phy.v
@@ -0,0 +1,95 @@
+// test_multiple_modules.phy.v
+
+module A (
+  in_A_one,
+  in_A_two,
+  in_A_three,
+  in_A_four,
+  out_A_one,
+  out_A_two,
+  out_A_three,
+  out_A,
+  VDD,
+  VSS
+);
+
+  inout VDD;
+  inout VSS;
+  input in_A_one;
+  input in_A_two;
+  input in_A_three;
+  input in_A_four;
+  output out_A_one;
+  output out_A_two;
+  output out_A_three;
+  output out_A;
+
+  // Internal wires
+  wire n101;
+  wire n102;
+  wire N101;
+  wire IN_A_ONE;
+
+  B mymodule1 (
+    .in_B_one(in_A_one),
+    .in_B_two(in_A_two),
+    .in_B_three(n102),
+    .in_B_four(in_A_three),
+    .out_B_one(N101),
+    .out_B_two(out_A),
+    .out_B_three(in_A_two),
+    .out_B(IN_A_ONE),
+    .VDD(VDD),
+    .VSS(VSS)
+  );
+
+endmodule
+
+
+module B (
+  in_B_one,
+  in_B_two,
+  in_B_three,
+  in_B_four,
+  out_B_one,
+  out_B_two,
+  out_B_three,
+  out_B,
+  VDD,
+  VSS
+);
+
+  inout VDD;
+  inout VSS;
+  input in_B_one;
+  input in_B_two;
+  input in_B_three;
+  input in_B_four;
+  output out_B_one;
+  output out_B_two;
+  output out_B_three;
+  output out_B;
+
+  // Internal wires
+  wire n732;
+  wire n164;
+  wire N300;
+  wire n300;
+
+  DEL01BWP7T FE_OFC1487_n164 (
+ .Z(N300),
+ .I(in_B_four),
+ .VDD(VDD),
+ .VSS(VSS));
+
+endmodule
+
+module C (
+VDD,
+VSS
+);
+
+  inout VDD;
+  inout VSS;
+
+endmodule
